207761d112d8356af7ac498c123480efacff29a3
Kévin VIVOR <kevin.vivor@appscho.com>
2024-10-17 15:36:44 +0200

    add logo and colors

--- app/src/main/java/com/kevinvi/mangadisplay/MainActivity.kt
+++ app/src/main/java/com/kevinvi/mangadisplay/MainActivity.kt
@@ -3,10 +3,13 @@ package com.kevinvi.mangadisplay
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.DisposableEffect
 import com.kevinvi.mangadisplay.navigation.MangaDisplayNavHost
 import com.kevinvi.mangadisplay.navigation.MangaDisplayNavigator
 import com.kevinvi.mangadisplay.ui.MangaDisplayApp
-import com.kevinvi.popular.ui.PopulareScreen
+import com.kevinvi.mangadisplay.ui.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,15 +17,23 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent{
+        setContent {
 
-            MangaDisplayApp { innerPadding, navigator ->
-                MangaDisplayNavHost(
-                    navController = navigator.navController,
-                    startScreen = MangaDisplayNavigator.startScreen,
-                    innerPadding = innerPadding,
-                )
+            // Update the dark content of the system bars to match the theme
+            DisposableEffect(isSystemInDarkTheme()) {
+                enableEdgeToEdge()
+                onDispose {}
             }
+            AppTheme(true, true) {
+                MangaDisplayApp { innerPadding, navigator ->
+                    MangaDisplayNavHost(
+                        navController = navigator.navController,
+                        startScreen = MangaDisplayNavigator.startScreen,
+                        innerPadding = innerPadding,
+                    )
+                }
+            }
+
         }
     }
 }
