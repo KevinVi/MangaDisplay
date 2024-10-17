6bf42ca53276172689d8e343b1d49a5cf03f6baa
Kévin VIVOR <kevin.vivor@appscho.com>
2024-10-18 01:53:04 +0200

    translate title and description

--- core/data/src/main/java/com/kevinvi/data/mapper/PopularItemMapper.kt
+++ core/data/src/main/java/com/kevinvi/data/mapper/PopularItemMapper.kt
@@ -2,6 +2,7 @@ package com.kevinvi.data.mapper
 
 import com.kevinvi.common.extensions.empty
 import com.kevinvi.data.model.PopularData
+import com.kevinvi.data.model.PopularDescription
 import com.kevinvi.data.model.PopularItemUI
 import com.kevinvi.data.model.PopularRelationships
 
@@ -10,9 +11,9 @@ object PopularItemMapper {
 
     fun mapToPopular(popularItem: PopularData) = PopularItemUI(
         id = popularItem.id,
-        title = popularItem.attributes?.title?.toString(),
+        title = getDescription(popularItem.attributes?.title),
         altTitles = popularItem.attributes?.altTitles?.toString(),
-        description = popularItem.attributes?.description?.toString(),
+        description = getDescription(popularItem.attributes?.description),
         createdAt = popularItem.attributes?.createdAt,
         updatedAt = popularItem.attributes?.updatedAt,
         image = getImage(popularItem.id, getCoverArt(popularItem.relationships)),
@@ -27,6 +28,15 @@ object PopularItemMapper {
         }
         return String.empty
     }
+
+    private fun getDescription(desc: PopularDescription?) =
+        if (!desc?.fr.isNullOrEmpty()) {
+            desc.fr
+        } else if (!desc?.en.isNullOrEmpty()) {
+            desc.en
+        } else {
+            String.empty
+        }
 
 
     private fun getImage(id: String, coverArt: String) =
